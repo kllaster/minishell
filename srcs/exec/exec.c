3e5fdf524781f3ddd555a32090aebdf144d0275e
Kllaster <noityt@mail.ru>
2022-04-20 22:00:18 +0300

    Fixed dup fd in builtin

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -1,10 +1,7 @@
 #include "minishell.h"
 
-static int wait_cmd(t_cmd* s_cmd)
+static void close_cmd_fd(t_cmd* s_cmd)
 {
-	int res;
-
-	res = wait_process(s_cmd->pid);
 	if (s_cmd->fd[STDIN_FILENO] != STDIN_FILENO)
 	{
 		close(s_cmd->fd[STDIN_FILENO]);
@@ -15,7 +12,6 @@ static int wait_cmd(t_cmd* s_cmd)
 		close(s_cmd->fd[STDOUT_FILENO]);
 		s_cmd->fd[STDOUT_FILENO] = STDOUT_FILENO;
 	}
-	return (res);
 }
 
 static int exec_cmd(t_cmd* s_cmd)
@@ -45,13 +41,15 @@ static int exec_cmd_fork(t_cmd* s_cmd)
 	else if (s_cmd->pid == 0)
 		exit(exec_cmd(s_cmd));
 
-	error = wait_cmd(s_cmd);
+	error = wait_process(s_cmd->pid);
+	close_cmd_fd(s_cmd);
 	return (error);
 }
 
 static int call_cmd(t_cmd* s_cmd)
 {
 	int res;
+	int saved_fd[2];
 
 	if (check_cmd(s_cmd) == 1)
 		return (1);
@@ -64,7 +62,16 @@ static int call_cmd(t_cmd* s_cmd)
 			res = 1;
 		}
 		else
+		{
+			saved_fd[0] = dup(0);
+			saved_fd[1] = dup(1);
 			res = exec_cmd(s_cmd);
+			close_cmd_fd(s_cmd);
+			if (dup_fd(saved_fd[0], STDIN_FILENO) != 0)
+				return (errno);
+			if (dup_fd(saved_fd[1], STDOUT_FILENO) != 0)
+				return (errno);
+		}
 	}
 	else
 		res = exec_cmd_fork(s_cmd);
